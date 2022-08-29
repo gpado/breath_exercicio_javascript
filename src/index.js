86c0c1fa690df8239f375c820550f533ee1066e4
GustavoHerreroNunes <ghnunes2010@gmail.com>
2022-08-29 17:01:28 -0300

    Add: Aplicação das duas animção e função que define o exercício de respiração

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const game = {
     initialize: () => {
-        let canvas = document.getElementById('Canvas');
+        game.getCanvasLayers();
+        breathExercise.initialize("Teste", 2, 4, 2, 5);
+        breathInterface.initialize();
+        game.draw();
+        game.update()
+    },
+
+    getCanvasLayers: () => {
+        let canvas = document.getElementById('firstCanvasLayer');
         let context = canvas.getContext('2d');
         let centerX = canvas.width / 2;
         let centerY = canvas.height / 2;
 
-        game.area = {
+        game.firstLayer = {
             canvas,
             context,
             center: {
@@ -14,20 +22,52 @@ const game = {
             }
         }
 
-        breathInterface.initialize();
+        canvas = document.getElementById('secondCanvasLayer');
+        context = canvas.getContext('2d');
+        centerX = canvas.width / 2;
+        centerY = canvas.height / 2;
 
-        game.draw();
+        game.secondLayer = {
+            canvas,
+            context,
+            center: {
+                x: centerX,
+                y: centerY
+            }
+        }
     },
 
     draw: () => {
-        let area = game.area;
+        let firstLayer = game.firstLayer;
+        let secondLayer = game.secondLayer;
         let circle = breathInterface.circle;
 
-        area.context.beginPath();
-        area.context.arc(area.center.x, area.center.y, circle.radius, 0, 2 * Math.PI, false);
-        area.context.fillStyle = circle.bgColor; //cor circulo   
-        area.context.fill();
-        area.context.lineWidth = circle.border;  //borda circulo 
+        firstLayer.context.beginPath();
+        firstLayer.context.arc(firstLayer.center.x, firstLayer.center.y, circle.radius, 0, 2 * Math.PI, false);
+        firstLayer.context.fillStyle = circle.bgColor; //cor circulo   
+        firstLayer.context.fill();
+
+        secondLayer.context.beginPath();
+        secondLayer.context.arc(secondLayer.center.x, secondLayer.center.y, circle.radius, 0, 2 * Math.PI, false);
+        secondLayer.context.strokeStyle = circle.borderColor;   
+        secondLayer.context.lineWidth = circle.border; //borda do círculo
+        secondLayer.context.stroke();
+
+        secondLayer.context.fillStyle = breathInterface.text.color;
+        secondLayer.context.textAlign = breathInterface.text.align;
+        secondLayer.context.font = breathInterface.text.font;
+        secondLayer.context.fillText("Pronto?", secondLayer.center.x, secondLayer.canvas.height - breathInterface.text.paddingBottom);
+    },
+
+    update: () => {
+        if(breathInterface.playState === "PLAYED"){
+            breathInterface.controlAnimation();
+            breathInterface.showMessage(game.secondLayer);
+            game.requestAnimationID = requestAnimationFrame(game.update);
+        }else{
+            cancelAnimationFrame(game.requestAnimationID);
+        }
+        
     }
 }
 
@@ -35,8 +75,18 @@ const breathInterface = {
     circle: {
         radius: 100,
         bgColor: "#596cb3",
-        border: 0
+        borderColor: "#3a53af",
+        border: 4
     },
+
+    text: {
+        color: "#3a53af",
+        align: "center",
+        font: "600 20px Roboto",
+        paddingBottom: 10
+    },
+
+    playState: "STOPPED",
 
     initialize: () => {
         let btnToggle = $("#btnToggle");
@@ -77,15 +127,80 @@ const breathInterface = {
     },
 
     playAnimation: () => {
-        $("canvas").css("animation", "mymove 8s infinite");
+        $("#firstCanvasLayer").css("animation", `inspire ${breathExercise.inspireDuration}s linear`);
+        
+        breathInterface.playState = 'PLAYED';
+        breathInterface.startAnimationTime = (new Date().getTime())/1000;
+        breathInterface.animationDuration = breathExercise.inspireDuration;
+        breathInterface.animationPlaying = "INSPIRE";
+        
+        game.requestAnimationID = requestAnimationFrame(game.update);
     },
 
     pauseAnimation: () => {
-        $("canvas").css("animation", "");
+        breathInterface.playState = 'PAUSED';
+        $("#firstCanvasLayer").css("animation", "");
+    },
+    
+    cancelAnimation: () => {
+        breathInterface.playState = 'STOPPED';
+        $("#firstCanvasLayer").css("animation", "");
     },
 
-    cancelAnimation: () => {
-        $("canvas").css("animation", "");
+    controlAnimation: () => {
+        let currentTime = (new Date().getTime())/1000;
+        let passedTime = currentTime - breathInterface.startAnimationTime;
+    
+        if(passedTime >= breathInterface.animationDuration){
+            switch(breathInterface.animationPlaying){
+                case "INSPIRE":
+                    breathInterface.animationDuration = breathExercise.delayDuration;
+                    breathInterface.animationPlaying = "DELAY";
+                    console.log("Aguarde");
+                    break;
+                case "DELAY":
+                    $("#firstCanvasLayer").css("animation", `expire ${breathExercise.expireDuration}s linear`);
+                    breathInterface.animationDuration = breathExercise.expireDuration;
+                    breathInterface.animationPlaying = "EXPIRE";
+                    console.log("Expire");
+                    break;
+                case "EXPIRE":
+                    $("#firstCanvasLayer").css("animation", `inspire ${breathExercise.inspireDuration}s linear`);
+                    breathInterface.animationDuration = breathExercise.inspireDuration;
+                    breathInterface.animationPlaying = "INSPIRE";
+                    console.log("Inspire");
+                    break;
+            }
+            breathInterface.startAnimationTime = (new Date().getTime())/1000;
+        }
+    },
+    
+    showMessage: (layer) => {
+        let message = "";
+        switch(breathInterface.animationPlaying){
+            case "INSPIRE":
+                message = "Inspire";
+                break;
+            case "DELAY":
+                message = "Segure";
+                break;
+            case "EXPIRE":
+                message = "Expire";
+                break;
+        }
+
+        layer.context.clearRect(0, layer.canvas.height - 30, layer.canvas.width, layer.canvas.height);
+        layer.context.fillText(message, layer.center.x, layer.canvas.height - breathInterface.text.paddingBottom);
+    }
+}
+
+const breathExercise = {
+    initialize: (name, numberOfCycles, inspireDuration, delayDuration, expireDuration) => {
+        breathExercise.name = name;
+        breathExercise.numberOfCycles = numberOfCycles;
+        breathExercise.inspireDuration = inspireDuration;
+        breathExercise.delayDuration = delayDuration;
+        breathExercise.expireDuration = expireDuration;
     }
 }
 
